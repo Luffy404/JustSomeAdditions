17808ed1b190bd306634ea18f5b823ec6595010f
Luffy404 <yassir.machaka@web.de>
2024-03-18 18:25:48 +0100

    FIX: Stacked Enchantments (#1)

--- src/main/java/de/madluffy/justsomeadditions/commands/uenchant.java
+++ src/main/java/de/madluffy/justsomeadditions/commands/uenchant.java
@@ -12,9 +12,11 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.List;
+import java.util.Map;
 
 public class uenchant {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher){
@@ -30,8 +32,8 @@ public class uenchant {
         dispatcher.register(command);
     }
     private static int addEnchantment(CommandContext<CommandSourceStack> command){
-        //
         int level = Integer.parseInt(command.getInput().replaceAll("[^0-9]", ""));
+        String enchantment = "enchantment.minecraft." + command.getNodes().get(1).getNode().getName();
         if (level <= 0 || level >= 128){
             command.getSource().getPlayer().sendSystemMessage(Component.literal("Invalid Range, choose a number between 1 and 127!")
                     .withStyle(ChatFormatting.RED), false);
@@ -43,10 +45,13 @@ public class uenchant {
             return 0;
         }
         for (Enchantment ench: ForgeRegistries.ENCHANTMENTS){
-            if (ench.getDescriptionId().equals("enchantment.minecraft." + command.getNodes().get(1).getNode().getName())){
-                    ItemStack item = command.getSource().getPlayer().getMainHandItem();
-                    // TODO: When used more than once it appends enchantments. Gotta remove specified enchantment and reapply
-                    item.enchant(ench, level);
+            if (ench.getDescriptionId().equals(enchantment)){
+                ItemStack item = command.getSource().getPlayer().getMainHandItem();
+                Map<Enchantment, Integer> enchantments = item.getAllEnchantments();
+                enchantments.remove(ench);
+                enchantments.put(ench, level);
+                EnchantmentHelper.setEnchantments(enchantments, item);
+                break;
             }
         }
         return Command.SINGLE_SUCCESS;
