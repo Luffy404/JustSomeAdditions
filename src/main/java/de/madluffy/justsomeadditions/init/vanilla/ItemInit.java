81d156b90d75b64a95de2e80dcd360fd36721876
Luffy404 <yassir.machaka@web.de>
2025-01-26 03:41:18 +0100

    Add: Totem of Undying now stacks to 64.

--- src/main/java/de/madluffy/justsomeadditions/init/vanilla/ItemInit.java
+++ src/main/java/de/madluffy/justsomeadditions/init/vanilla/ItemInit.java
@@ -2,6 +2,7 @@ package de.madluffy.justsomeadditions.init.vanilla;
 
 import de.madluffy.justsomeadditions.items.InfinityBowItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -12,5 +13,7 @@ public class ItemInit {
 
     public static final RegistryObject<Item> BOW = VANILLA_ITEMS.register("bow",
             () -> new InfinityBowItem(new Item.Properties().durability(384)));
+    public static final RegistryObject<Item> TOTEM_OF_UNDYING = VANILLA_ITEMS.register("totem_of_undying",
+            () -> new InfinityBowItem(new Item.Properties().stacksTo(64).rarity(Rarity.UNCOMMON)));
 
 }
